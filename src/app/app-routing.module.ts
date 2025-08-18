0c23077a0da0071ab6e32ed403e34eebc2d160d6
Rayan Alolyani <ryyanoly@gmail.com>
2025-08-18 11:51:11 +0300

    prepare and clean files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { WorkComponent } from './pages/work/work.component';
-import { ArticlsComponent } from './pages/articls/articls.component';
 import { CertificationsComponent } from './pages/certifications/certifications.component';
-import { AboutMeComponent } from './pages/about-me/about-me.component';
-import { ServicesComponent } from './pages/services/services.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { Certificate } from './models/certificate.model';
 import { MainComponent } from './main/main.component';
+import { NgOptimizedImage } from '@angular/common';
 const routes: Routes = [
   { path: '', component: MainComponent }, // Default route
   { path: 'main', component: MainComponent },
-  { path: 'about', component: AboutMeComponent }, // About Me
   { path: 'work', component: WorkComponent }, // Work
   { path: 'certifications', component: CertificationsComponent }, // Certifications
   { path: 'projects', component: ProjectsComponent },
@@ -25,7 +22,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, {
     scrollPositionRestoration: 'enabled', // <-- يرجع لأعلى الصفحة
     anchorScrolling: 'enabled'            // <-- في حال تستخدم روابط داخلية #
-  })],
+  }) ,NgOptimizedImage],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
